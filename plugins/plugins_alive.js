4f23444b53c3aeaaa50ddf80648328935e7ae723
➳Chethiya_MD╰‿╯穴༺ <chethiyamd@gmail.com>
2025-07-08 12:32:00 +0530

    Update plugins_alive.js

--- plugins/plugins_alive.js
+++ plugins/plugins_alive.js
@@ -1,4 +1,4 @@
-const config = require("../config");
+const config = require("../settings");
 const { cmd } = require('../lib/command');
 const os = require('os');
 const { getBuffer, runtime } = require('../lib/functions');
