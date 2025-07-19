c81cb060b8de77450f39a28a34ee3e0d287e41be
➳Chethiya_MD╰‿╯穴༺ <chethiyamd@gmail.com>
2025-07-19 22:25:03 +0530

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -1,9 +1,26 @@
-const { cmd } = require("../command");
-const { proto, generateWAMessageFromContent, prepareWAMessageMedia } = require("@whiskeysockets/baileys");
+const os = require("os");
+const { cmd, commands } = require("../lib/command");
+var { get_set, input_set } = require("../lib/set_db");
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, Func, fetchJson, } = require("../lib/functions");
+var {
+  updateCMDStore,
+  isbtnID,
+  getCMDStore,
+  getCmdForCmdId,
+  connectdb,
+  input,
+  get,
+  updb,
+  updfb
+} = require("../lib/database");
+const axios = require("axios");
+const fs = require('fs');
+const config = require("../settings");
+const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, getBinaryNodeChildren, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType, downloadContentFromMessage} = require('@whiskeysockets/baileys');
 
 cmd({
-  pattern: "chetiyamenu",
-  alias: ["menul", "cmd"],
+  pattern: "menu",
+  alias: ["menul", "md"],
   desc: "Chetiya md Stylish Interactive Menu",
   react: "🧨",
   category: "utility",
