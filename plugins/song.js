210a6f6ecef03aae87c4a9e2d6152a3f89313567
➳Chethiya_MD╰‿╯穴༺ <chethiyamd@gmail.com>
2025-07-19 14:58:16 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -1,14 +1,29 @@
-const axios = require('axios');
-const { cmd } = require('../command');
-const { fetchJson } = require('../lib/functions');
-
+const os = require("os");
+const { cmd, commands } = require("../lib/command");
+var { get_set, input_set } = require("../lib/set_db");
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, Func, fetchJson, } = require("../lib/functions");
+var {
+  updateCMDStore,
+  isbtnID,
+  getCMDStore,
+  getCmdForCmdId,
+  connectdb,
+  input,
+  get,
+  updb,
+  updfb
+} = require("../lib/database");
+const axios = require("axios");
+const fs = require('fs');
+const config = require("../settings");
+const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, getBinaryNodeChildren, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType, downloadContentFromMessage} = require('@whiskeysockets/baileys');
 cmd({
     pattern: "news",
     alias: ["hirunews", "newshiru", "hirulk"],
     react: "⭐",
     category: "search hiru news",
     desc: "Fetch the latest news from the SUHAS API in Hiru API.",
-    use: ".news",
+    use: "",
     filename: __filename,
 },
     async (conn, mek, m, {
