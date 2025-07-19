a2c083c8204b646ca2e664270a7651f75ec3db2d
➳Chethiya_MD╰‿╯穴༺ <chethiyamd@gmail.com>
2025-07-19 11:09:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Chetiya=F2dGmbrD#Lt6jJR5epBAiMai1ZSW74AFMRYDz245CsWnsFW1nNjo' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Chetiya=1nEDGCCC#SChyhdmjUcSinOdKYIXdRP__Qj0lc5wiRXlWR3G5TRQ' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
